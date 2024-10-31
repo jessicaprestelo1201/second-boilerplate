07186b59ca735bdd228f2efa8c4898bbba32073a
Jéssica Prestelo <jessica.p.jesus@aluno.senai.br>
2024-10-31 10:50:13 -0300

    feat: create Get by id method

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -28,4 +28,27 @@ usuario,
 })
 
 })
+usuariosRoutes.get("/:id", (req, res) => {
+const {id} = req.params;
+const user = usersRepository.getUserById(id);
+
+if (!user) {
+    return res.status(404).json({
+        message: `Usuário com id ${id} não encontrado!`
+    });
+}
+return res.status(200).json ({
+    message: `Usuário com id ${id} encontrado!`,
+    user,
+})
+});
+usuariosRoutes.put("/:id", (req, res) => {
+
+})
+usuariosRoutes.delete("/:id", (req, res) => {
+
+})
+
+
+
 export default usuariosRoutes;
